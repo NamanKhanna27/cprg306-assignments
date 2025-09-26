ab52f3bf92d22b3d115bd2c6f7d5598df97d42c8
NamanKhanna27 <namank27102004@gmail.com>
2025-09-25 21:43:10 -0600

    Added Week 3 page link

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function RootPage() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to Week 2 Page</Link>
+      <Link href="https://sd-web-2.vercel.app/week-2">Go to Week 2 Page</Link>
+      <Link href="https://sd-web-2.vercel.app/week-3">Go to Week 3 Page</Link>
     </div>
   );
 }
