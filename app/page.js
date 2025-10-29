aa49e1d905797a2bc159320245fa699c9043d1e0
NamanKhanna27 <namank27102004@gmail.com>
2025-10-29 03:24:36 -0600

    update page.js with linking week-8 file to it

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function RootPage() {
       <Link href="/week-5"> Go to Week 5 Page</Link>
       <Link href="/week-6"> Go to Week 6 Page</Link>
       <Link href="/week-7"> Go to Week 7 Page</Link>
+      <Link href="/week-8"> Go to Week 8 Page</Link>
     </div>
   );
 }
